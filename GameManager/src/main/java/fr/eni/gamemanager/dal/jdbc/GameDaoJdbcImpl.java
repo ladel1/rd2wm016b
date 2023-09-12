81a7833d996a9bf408de291b649f3979a8d6ceae
Adel LATIBI <alatibi@campus-eni.fr>
2023-09-12 10:20:38 +0200

    Find by name

--- GameManager/src/main/java/fr/eni/gamemanager/dal/jdbc/GameDaoJdbcImpl.java
+++ GameManager/src/main/java/fr/eni/gamemanager/dal/jdbc/GameDaoJdbcImpl.java
@@ -19,6 +19,8 @@ public class GameDaoJdbcImpl implements GameDao {
 	private static final String SELECT_ONE = "SELECT * FROM games WHERE id = ?";
 	private static final String SAVE = "INSERT games (name,company,category,price,releaseDate,age,format,version) VALUES (?,?,?,?,?,?,?,?)";
 	private static final String DELETE_ONE = "DELETE games WHERE id = ?";
+	private static final String UPDATE = "UPDATE games SET name=?,company=?,category=?,price=?,releaseDate=?,age=?,format=?,version=? WHERE id = ?";
+	private static final String FIND_BY_NAME = "SELECT * FROM games WHERE name LIKE ? ";
 	
 	
 	@Override
@@ -92,8 +94,28 @@ public class GameDaoJdbcImpl implements GameDao {
 
 	@Override
 	public void modify(Game game) {
-		// TODO Auto-generated method stub
-		
+		try(
+				Connection connection = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = connection.prepareStatement(UPDATE)
+				){
+			
+			// Update Set
+			pstmt.setString(1, game.getName());
+			pstmt.setString(2, game.getCompany());
+			pstmt.setString(3, game.getCategory());
+			pstmt.setFloat(4, game.getPrice());
+			pstmt.setDate(5, Date.valueOf(game.getReleaseDate()) );
+			pstmt.setInt(6, game.getAge());
+			pstmt.setString(7, game.getFormat());
+			pstmt.setString(8, game.getVersion());
+			// Where id
+			pstmt.setInt(9, game.getId());
+			// execute
+			pstmt.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -112,7 +134,28 @@ public class GameDaoJdbcImpl implements GameDao {
 
 	@Override
 	public List<Game> findByName(String query) {
-		// TODO Auto-generated method stub
+		try(
+				Connection connection = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = connection.prepareStatement(FIND_BY_NAME)
+			){
+			pstmt.setString(1, "%"+query+"%");
+			List<Game> games = new ArrayList<Game>();			
+			ResultSet rs =  pstmt.executeQuery();
+			while(rs.next()) {
+				games.add(
+						
+						new Game(rs.getInt("id"), rs.getString("name"), 
+								rs.getString("company"), rs.getString("category"), 
+								rs.getFloat("price"), rs.getDate("releaseDate").toLocalDate(), 
+								rs.getInt("age"), rs.getString("format"), 
+								rs.getString("version"))
+						
+						);				
+			}
+			return games;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}		
 		return null;
 	}
 
--- GameManager/src/main/java/fr/eni/gamemanager/ihm/EditGameServlet.java
+++ GameManager/src/main/java/fr/eni/gamemanager/ihm/EditGameServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 
 import fr.eni.gamemanager.bll.GameManager;
 import fr.eni.gamemanager.bo.Game;
@@ -32,7 +33,28 @@ public class EditGameServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		try {
+			// récupérer le param dans url
+			int id = Integer.parseInt( request.getParameter("id")  );
+			// get data from param
+			String name =   request.getParameter("name");
+			String company =   request.getParameter("company");
+			String cat =   request.getParameter("cat");
+			float price =  Float.parseFloat(request.getParameter("price")) ;
+			LocalDate releaseDate =  LocalDate.parse(request.getParameter("releaseDate")) ;
+			int pegi =  Integer.parseInt(request.getParameter("pegi")) ;
+			String format =   request.getParameter("format");
+			String version =   request.getParameter("version");			
+			// create Game instance
+			Game game = new Game(id, name, company, cat, price, releaseDate, pegi, format, version);
+			
+			// modifier
+			GameManager.getInstance().modifierUnJeu(game);
+			// redirect
+			response.sendRedirect( request.getContextPath() +"/jeux");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
